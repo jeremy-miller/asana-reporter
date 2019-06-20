0e3f4c4edc895051cd041e598e5824a6c00e8fd8
Jeremy Miller <jgmiller88@gmail.com>
2019-06-20 07:46:04 -0700

    Print yesterday's completed tasks

--- src/main.rs
+++ src/main.rs
@@ -43,16 +43,26 @@ fn main() -> Result<(), reqwest::Error> {
         .header("Authorization", bearer_header_value.clone())
         .send()?
         .json()?;
+    let mut all_completed_tasks: Vec<Task> = Vec::new();
     for project in project_response.data.into_iter() {
-        let yesterday = Utc::now() - Duration::days(1);
-        let yesterday = yesterday.format("%Y-%m-%dT%H:%M:%S");
-        let task_url = format!("https://app.asana.com/api/1.0/projects/{}/tasks?opt_fields=name,completed,completed_at&completed_since={}", project.gid, yesterday);
+        let completed_since = Utc::now() - Duration::days(1);
+        let completed_since = completed_since.format("%Y-%m-%dT%H:%M:%S");
+        let task_url = format!("https://app.asana.com/api/1.0/projects/{}/tasks?opt_fields=name,completed,completed_at&completed_since={}", project.gid, completed_since);
         let task_response: TaskResponse = client
             .get(&task_url)
             .header("Authorization", bearer_header_value.clone())
             .send()?
             .json()?;
-        println!("{:?}", task_response);
+        let completed_tasks: Vec<Task> = task_response
+            .data
+            .into_iter()
+            .filter(|task| task.completed)
+            .collect();
+        all_completed_tasks.extend(completed_tasks);
+    }
+    all_completed_tasks.sort_by_key(|task| task.completed_at);
+    for task in all_completed_tasks {
+        println!("- {}", task.name);
     }
     Ok(())
 }
